d819432f972b4382134aeeb6fa69884b52372043
simon <sjtucker28@gmail.com>
2020-02-24 17:52:55 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(line => {
+    let words = line.split(' ')
+    let capWord = 
+      words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    let response = capWord.join(' ')
+    return response
+  })
 }
